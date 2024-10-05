6ca4a78acffb0b05212b1f8d4334a2204c6eca7b
KalabAmssalu <Kalishdesandy@gmail.com>
2024-10-05 04:28:20 +0300

    feat(ui): system layout ui setting

---
+++ src/app/[locale]/(Dashboard)/organization/recipiant/page.tsx
@@ -0,0 +1,13 @@
+import RecipentScreen from "@/components/screen/organization-Screen/RecipentScreen";
+
+type Props = {};
+
+const Recipiantpage = (props: Props) => {
+	return (
+		<div>
+			<RecipentScreen />
+		</div>
+	);
+};
+
+export default Recipiantpage;
